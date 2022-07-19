1660ea3c73444fb04c7462efe3cf83ad7c02b3ec
valquiria <valqviria@gmail.com>
2022-07-19 17:47:47 -0300

    Set UseCase

--- app/src/main/java/com/valquiria/dndspells/domain/GetSpellListUseCase.kt
+++ app/src/main/java/com/valquiria/dndspells/domain/GetSpellListUseCase.kt
@@ -1,4 +1,11 @@
 package com.valquiria.dndspells.domain
 
-class GetSpellListUseCase {
+import com.valquiria.dndspells.data.database.entity.SpellEntity
+import com.valquiria.dndspells.data.repository.SpellRepository
+
+class GetSpellListUseCase(
+    private val repository: SpellRepository
+) {
+    suspend fun execute(): MutableList<SpellEntity> =
+        repository.loadSpells()
 }
