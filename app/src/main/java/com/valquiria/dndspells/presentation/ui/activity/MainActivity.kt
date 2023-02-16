9bc41e839c8475169b60bdb70af0cac866af145a
valquiria <valqviria@gmail.com>
2023-02-15 21:37:29 -0300

    Add toolbar and navigation button

--- app/src/main/java/com/valquiria/dndspells/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/valquiria/dndspells/presentation/ui/activity/MainActivity.kt
@@ -2,6 +2,9 @@ package com.valquiria.dndspells.presentation.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.valquiria.dndspells.R
 import com.valquiria.dndspells.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavigation()
     }
 
+    private fun setupNavigation() {
+        supportFragmentManager.findFragmentById(R.id.fragment_container_view)?.let {
+            NavigationUI.setupWithNavController(
+                binding.toolbar,
+                it.findNavController()
+                )
+        }
+    }
 }
